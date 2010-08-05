99da71a3f6cef47ecb39875a6810c45595089036
dizzydevil <stephan_lesch@users.sourceforge.net>
2010-08-05 11:08:08 +0000

    removed development paths from tagtool.bat

--- tagtool.bat
+++ tagtool.bat
@@ -12,11 +12,6 @@ set WXLUA_BIN=%PATH_NXOEDITOR%\application
 REM Lua's search path
 set LUA_PATH=%PATH_NXOEDITOR%\nxo_editor\?.lua;%PATH_NXOEDITOR%\application\lua\?.lua;%PATH_NXOEDITOR%\application\lua_hilscher\?.lua
 
-REM Development
-set PATH_NXOEDITOR=d:\projekt\Muhkuh\
-set WXLUA_BIN=d:\projekt\muhkuh_sf\bin
-set LUA_PATH=%PATH_NXOEDITOR%\nxo_editor\?.lua;%PATH_NXOEDITOR%\lua\?.lua;%PATH_NXOEDITOR%\lua_hilscher\?.lua
-
 REM call wxlua with the command args supplied; works only on win NT 4 and above
 "%WXLUA_BIN%\lua.exe" "%PATH_NXOEDITOR%\nxo_editor\tagtool.wx.lua" %*
 
