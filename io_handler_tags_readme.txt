dfd9039d649a7a894cc075efb0c735054ef4dc6e
dizzydevil <stephan_lesch@users.sourceforge.net>
2011-05-17 09:45:45 +0000

    added batch file to zip tagdefs_io_handler and the associated help files

---
+++ io_handler_tags_readme.txt
@@ -0,0 +1,2 @@
+In order to use the netPLC I/O Handler tags, unzip io_handler_tags.zip 
+to <install directory>/nxo_editor.
---
+++ zip_io_handler.bat
@@ -0,0 +1,3 @@
+C:\programme\7-Zip\7z.exe a -tzip io_handler_tags tagdefs_io_handler.lua help/netplc_io_handler/*.htm
+pause
+
