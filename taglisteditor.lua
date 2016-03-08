8c8d4535ce96966c359529d0d26def6cffa13b07
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2016-03-08 12:10:38 +0100

    Use "require" instead of "muhkuh.include".

--- taglisteditor.lua
+++ taglisteditor.lua
@@ -1,5 +1,6 @@
 require("muhkuh_cli_init")
 require("wx")
+require("nxoeditor")
 
 -- Create the main Frame.
 m_tMainFrame = wx.wxFrame(wx.NULL, wx.wxID_ANY, "netX Tag List Editor/NXO Builder 1.2.0.1", wx.wxDefaultPosition, wx.wxSize(640,480), wx.wxDEFAULT_FRAME_STYLE)
@@ -10,7 +11,6 @@ m_tMainFrame:Show(true)
 _G.__MUHKUH_PANEL = m_tPanel
 
 
-muhkuh.include("nxoeditor.lua", "nxoeditor")
 nxoeditor.run()
 
 wx.wxGetApp():MainLoop()
